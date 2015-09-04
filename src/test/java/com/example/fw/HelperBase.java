776ed598a6ba2d9327af939fa7bad47cedb3bd8c
Mikhail Shchegolev <mikhail.shchegolev@nordigy.ru>
2015-09-04 21:10:19 +0300

    add new package and class

---
+++ src/test/java/com/example/fw/HelperBase.java
@@ -0,0 +1,12 @@
+package com.example.fw;
+
+/**
+ * Created by mikhail.shchegolev on 04.09.2015.
+ */
+public class HelperBase {
+    private ApplicationManager manager;
+    public HelperBase(ApplicationManager manager){
+        this.manager = manager;
+
+    }
+}
